9d568db28516b94d1b2df055113df5f48284fa00
alshaboti <alshaboti.it@gmail.com>
2019-02-20 00:46:22 +1300

    plotme.py uses results.py and other two functions to draw figures

---
+++ Results.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+from __future__ import division, print_function
+import pandas as pd
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+
+# Set some parameters to apply to all plots. These can be overridden
+# in each plot if desired
+import matplotlib
+# Plot size to 14" x 7"
+matplotlib.rc('figure', figsize = (14, 7))
+# Font size to 14
+matplotlib.rc('font', size = 14)
+# Do not display top and right frame lines
+matplotlib.rc('axes.spines', top = False, right = False)
+# Remove grid lines
+matplotlib.rc('axes', grid = False)
+# Set backgound color to white
+matplotlib.rc('axes', facecolor = 'white')
+
+class OutputResult:
+    def __init__(self, file_name, header_row=None, sep="$"):
+
+        self.file_name = file_name
+        self.path = '/'.join(file_name.split('/')[0:-1])
+        self.sep = sep        
+
+        if header_row is not None and \
+             not os.path.isfile(self.file_name):
+                try:
+                    with open(self.file_name,'w+') as f:
+                        f.write(header_row+"\n") 
+                except IOError as e:
+                    print("Can't create ", self.file_name)              
+            
+        # Define a function to create a boxplot:
+    def write_results(self, new_row):
+        try:
+            with open(self.file_name,'a+') as f:
+                f.write(new_row+"\n") 
+        except IOError as e:
+            print("Can't open ", self.file_name)
+    
+    def _boxplot(self, x_data, y_data, base_color, median_color, x_label, y_label, title):
+        _, ax = plt.subplots()
+
+        # Draw boxplots, specifying desired style
+        ax.boxplot(y_data
+                    # patch_artist must be True to control box fill
+                    , patch_artist = True
+                    # Properties of median line
+                    , medianprops = {'color': median_color}
+                    # Properties of box
+                    , boxprops = {'color': base_color, 'facecolor': base_color}
+                    # Properties of whiskers
+                    , whiskerprops = {'color': base_color}
+                    # Properties of whisker caps
+                    , capprops = {'color': base_color})
+
+        # By default, the tick label starts at 1 and increments by 1 for
+        # each box drawn. This sets the labels to the ones we want
+        ax.set_xticklabels(x_data)
+        ax.set_ylabel(y_label)
+        ax.set_xlabel(x_label)
+        ax.set_title(title)   
+       
+    def create_figures(self, from_row, to_row):
+        data = pd.read_csv(self.file_name, self.sep)
+
+        score_headers = ["SA_score" , "HC_score" , "GA_score"]
+        x_headers = ["SA", "HC", "GA"]
+        task_len =  data['task_len'][from_row]
+        # score figure        
+        score_data = [data[x][from_row:to_row].values for x in score_headers]
+        x_label = 'Search algorithm (no functions per task = {0}, no alterernative devices ={1} )'.format( \
+                    data['task_len'][from_row], \
+                    data['dev_alter'][from_row])
+        y_label = 'User preference probability'
+        
+        self._boxplot(x_data = score_headers
+                , y_data = score_data
+                , base_color = '#539caf'
+                , median_color = '#297083'
+                , x_label = x_label
+                , y_label = y_label
+                , title = 'User preference score')
+        # save figure
+        plt.savefig(self.path +'/' +y_label+x_label+str(task_len)+".png")        
+
+        # time figure
+        time_headers = ["BF_time" , "SA_time" , "HC_time", "GA_time"  ]
+        time_data = [data[x][from_row:to_row].values for x in time_headers]
+        x_label = 'Search algorithm (no functions per task = {0}, no alterernative devices ={1} )'.format( \
+                    data['task_len'][from_row], \
+                    data['dev_alter'][from_row])
+        y_label = 'Time in seconds'        
+        # # Call the function to create plot
+        self._boxplot(x_data = x_headers
+                , y_data = time_data
+                , base_color = '#539caf'
+                , median_color = '#297083'
+                , x_label = x_label
+                , y_label = y_label
+                , title = 'Searching time')
+        # # save figure
+        plt.savefig(self.path +'/' +y_label+x_label+str(task_len)+".png")   
+        plt.close()
+
+res = OutputResult('./results.csv')
+
+exp_iterations = [i*30 for i in range(6)]
+
+for i in exp_iterations:
+    res.create_figures(i, i*30-1)
---
+++ plotme.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from Results import OutputResult
+
+
+#task_len$BrutForce$SA_score$h_score$ga_score
+path = './results.csv'
+data = pd.read_csv(path,sep='$')
+
+
+ 
+data_all = data.groupby('task_len')
+
+tasks_len = data_all.groups.keys()
+
+def score_figures():
+        algorithms_list = ['HC_score','SA_score','GA_score']
+        fig, axes = plt.subplots(nrows=1,ncols=6, figsize=(27,5))
+        plt.subplots_adjust(left= 0.1, bottom=0.1, right=0.9, top=0.8, wspace=0.5, hspace=0.2)
+
+        # fro each task_len get all scores
+        for plt_idx, task_length in enumerate(tasks_len):
+
+                task_scores = []
+                # get all scores for this task_len
+                for alg in algorithms_list:
+                        data_alg = data_all[alg].apply(list)    
+                        task_scores.append( data_alg[task_length] )
+
+                axes[plt_idx].violinplot(task_scores)
+                # axes[plt_idx].boxplot(task_scores)
+
+                # draw optimal score    
+                up_score = data_all['UP_score'].apply(list)[task_length][0]
+                axes[plt_idx].axhline(y=up_score, color='r', linestyle='-.', linewidth=0.2)
+                
+                # ygrid and xlable
+                axes[plt_idx].yaxis.grid(True)    
+                axes[plt_idx].set_xlabel('Task len='+str(task_length))    
+        
+        # Ylabel for the first subplot
+        axes[0].set_ylabel('User preference score')
+
+        # add x-tick labels
+        plt.setp(axes, xticks=[1, 2,3],
+                xticklabels=[s.split('_')[0] for s in algorithms_list])
+        fig.suptitle('User preference search algorithms', fontsize=12)
+
+        #plt.show()
+        plt.savefig("./search_score.png")  
+        plt.close()
+
+# make it one function
+def time_figures():
+        algorithms_list = ['HC_time','SA_time','GA_time']
+        fig, axes = plt.subplots(nrows=1,ncols=6, figsize=(27,5))
+        plt.subplots_adjust(left= 0.1, bottom=0.1, right=0.9, top=0.8, wspace=0.5, hspace=0.2)
+
+        # fro each task_len get all scores
+        for plt_idx, task_length in enumerate(tasks_len):
+
+                task_scores = []
+                # get all scores for this task_len
+                for alg in algorithms_list:
+                        data_alg = data_all[alg].apply(list)    
+                        task_scores.append( data_alg[task_length] )
+
+                axes[plt_idx].violinplot(task_scores)
+                # axes[plt_idx].boxplot(task_scores)
+
+                # draw optimal score    
+                up_score = data_all['BF_time'].apply(list)[task_length][0]
+                axes[plt_idx].axhline(y=up_score, color='r', linestyle='-.', linewidth=0.2)
+                
+                # ygrid and xlable
+                axes[plt_idx].yaxis.grid(True)    
+                axes[plt_idx].set_xlabel('Task len='+str(task_length))    
+        
+        # Ylabel for the first subplot
+        axes[0].set_ylabel('Elapsed time')
+
+        # add x-tick labels
+        plt.setp(axes, xticks=[1, 2,3],
+                xticklabels=[s.split('_')[0] for s in algorithms_list])
+        fig.suptitle('Elapsed time for searching algorithms', fontsize=12)
+
+        #plt.show()
+        plt.savefig("./search_time.png")  
+        plt.close()
+
+if __name__ == "__main__":
+        score_figures()
+        time_figures()
+  
+        res = OutputResult('./results.csv')
+        exp_iterations = [i*30 for i in range(6)]
+
+        for i in exp_iterations:
+                res.create_figures(i, i*30-1)
